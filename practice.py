705eb0068e161a457d77eb4fd638dee9e3e41318
a3013998769 <1042773983@qq.com>
2016-11-17 10:37:17 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,10 +1,13 @@
-print("Welcome to my number guessing game.")
-print("For what number which between 20 and 100 would you like to multiply?")
+print("Welcome to my numberical guessing game.")
+print("For what number which between 1 and 1000 would you like to multiply?")
 number = int(input())
+print("For what number do you want to be the upper limit of the multiplication?")
+upper_limit = int(input())
 times = 0
-while number > 20 and number < 100 and number * times < 1000:
+while number >= 1 and number <= 1000 and number * times <= upper_limit:
 	number2 = number * times
 	print(str(times) + " times " + str(number) + " equal " + str(number2))
 	times = int(times + 1)
-if number2 > 1000:
+if number * times >= upper_limit:
 	print("Thanks for using my program!")
+	print("There are " + str(times) + " integer multiples of " + str(number) + " that are less than " + str(upper_limit) + ".")
