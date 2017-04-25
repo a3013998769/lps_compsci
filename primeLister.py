4efc922f7f81190bf65bba0421423564627d61be
a3013998769 <1042773983@qq.com>
2017-04-25 10:07:47 -0700

    Update primeLister.py

--- primeLister.py
+++ primeLister.py
@@ -1,11 +1,10 @@
-class number(object):
-  def prime(n):
+def prime(n):
     list = []
     x = range(2,n)
     for i in x:
-      y = n % i
-      if i == 0:
-        list.append(n)
+        y = n % i
+        if i == 0:
+            list.append(n)
 print("welcome to the primelister!")
 print("In this program, it will list all of the prime numbers between 2 and 10000.")
 number = range(2,10000)
